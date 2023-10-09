fb2f3b715cfc1fb7cf3c4544ed33c669eb778634
Xeu <thankrain@qq.com>
2023-10-09 20:09:26 +0800

    [feat] support user define repo url

--- install.sh
+++ install.sh
@@ -2,11 +2,21 @@
 
 # 定义仓库地址
 # 根据环境变量 SHELLOVE_TYPE 决定仓库地址
-if [ "$SHELLOVE_TYPE" = "ssh" ]; then
-    repository_url="git@github.com:ThankRain/shellove.git"
+# 提示用户输入远程仓库地址，同时提供默认值
+read -p "请输入远程仓库地址(https://github.com/ThankRain/shellove.git): " user_input
+
+# 如果用户输入为空，则使用默认仓库地址1
+if [ -z "$user_input" ]; then
+  repository_url="https://github.com/ThankRain/shellove.git"
+# 如果用户输入为"ssh"，则使用仓库地址2
+elif [ "$user_input" = "ssh" ]; then
+  repository_url="git@github.com:ThankRain/shellove.git"
 else
-    repository_url="https://github.com/ThankRain/shellove.git"
+  repository_url="$user_input"
 fi
+
+# 输出最终选择的仓库地址
+echo "选择的远程仓库地址是：$repository_url"
 
 # 检查是否已经安装过脚本
 if [ -d "$HOME/.scripts" ]; then
